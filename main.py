ac93a79a4280001305339e1e802d75ae20e9bdea
HoopDead <polec0@wp.pl>
2020-08-13 01:16:37 +0200

    Added a function for shift matrix when move up

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import numpy as np
+
 
 
 driver = webdriver.Chrome('/mnt/d/Programs/Projekty/2048_player/chromedriver.exe')
@@ -31,12 +34,46 @@ def modify_list_of_tiles(list_of_tiles):
         for r in (("tile", ""), ("new", ""), ("merged", ""), ("-", ""), ("position", ""), (" ", "")):
             element = element.replace(*r)
         list_of_tile_informations[int(element[1])-1][int(element[2])-1] = int(element[0])
-    print(list_of_tile_informations)
     return list_of_tile_informations
 
 
+def check_best_move(list_of_tile_informations):
+    np_list_of_tile_informations = np.array(list_of_tile_informations)
+
+def check_move_up(list_of_tile_informations):
+    for i in range(len(list_of_tile_informations)):
+        n = len(list_of_tile_informations)
+        v = []
+        w = []
+        for j in range(n - 1, -1, -1):
+            if(list_of_tile_informations[i][j]):
+                v.append(list_of_tile_informations[i][j])
+
+        j = 0
+        while (j < len(v)): 
+                if (j < len(v) - 1 and v[j] == v[j + 1]): 
+                    w.append(2 * v[j]) 
+                    j += 1
+                else: 
+                    w.append(v[j])
+
+                j += 1
+                for j in range(n): 
+                    list_of_tile_informations[i][j] = 0
+  
+                j = n - 1
+                for it in w: 
+                    list_of_tile_informations[i][j] = it 
+                    j -= 1
+    print(list_of_tile_informations)
+    
+Game = True
+
 if __name__ == "__main__":
     print("Im working!")
-    list_of_tiles = get_values_from_tiles()
-    list_of_tile_informations = modify_list_of_tiles(list_of_tiles)
-    print(list_of_tile_informations)
+    while Game:
+        list_of_tiles = get_values_from_tiles()
+        list_of_tile_informations = modify_list_of_tiles(list_of_tiles)
+        main_window = driver.find_element_by_tag_name("body")
+        check_best_move(list_of_tile_informations)
+        check_move_up(list_of_tile_informations)
